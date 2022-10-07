88db1143dbf09d068373d51601d0408c936ea545
Akıntürk Arınç Aslan <akinturk.aslan@SmartSinif.local>
2022-10-07 18:36:36 +0300

    il commit

---
+++ src/Project/MainPage/MainPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { useState, useEffect } from 'react';
+import Navbar from '../../Tools/Navbar/Navbar';
+
+
+const MainPage = () =>{
+
+
+
+
+
+    return(
+
+            <Navbar/>
+
+    );
+}
+
+export default MainPage;
